708c144097700560dc77a8e808472d7550ad44b7
Agustín Rodríguez <me@agurodriguez.net>
2019-09-17 22:24:47 -0300

    Updates

---
+++ precache-manifest.d3f4b89e292efe22504ff505680284b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "14bba6e8597f0424f8fb",
+    "url": "/static/js/main.14bba6e8.chunk.js"
+  },
+  {
+    "revision": "26f676abaf5fb8f6d5a7",
+    "url": "/static/js/1.26f676ab.chunk.js"
+  },
+  {
+    "revision": "14bba6e8597f0424f8fb",
+    "url": "/static/css/main.b36c054a.chunk.css"
+  },
+  {
+    "revision": "26f676abaf5fb8f6d5a7",
+    "url": "/static/css/1.8670a012.chunk.css"
+  },
+  {
+    "revision": "eb42f6e7c3e74427106cb00b72e98243",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50b9d9d0ea936ba2e20034160b6c7eb3.js"
+  "/precache-manifest.d3f4b89e292efe22504ff505680284b8.js"
 );
 
 workbox.clientsClaim();
